f656ad2d5821235250cfc9245f801929f25df13b
Al Hunt <al@returnzero.org>
2017-01-04 19:21:37 +0800

    Implemented main.c

--- 298-easy/src/main.c
+++ 298-easy/src/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #include <parentheses.h>
 
 int main(int argc, char* argv[]) {
+	char dupes[255];
+
+	printf("Enter expression you wish to simplify: ");
+	scanf("%s", &dupes);
+	rem_dupes(dupes, strlen(dupes));
+	printf("%s\n", dupes);
+
 	return 0;
 }
