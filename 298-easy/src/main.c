0611a93020a29cbee8f57a54b4afaacfe3def9ce
Al Hunt <al@returnzero.org>
2017-01-04 19:36:49 +0800

    Changed main.c to use fgets

--- 298-easy/src/main.c
+++ 298-easy/src/main.c
@@ -4,11 +4,15 @@
 
 int main(int argc, char* argv[]) {
 	char dupes[255];
+	int len;
 
 	printf("Enter expression you wish to simplify: ");
-	scanf("%s", &dupes);
-	rem_dupes(dupes, strlen(dupes));
-	printf("%s\n", dupes);
+	fgets(dupes, 255, stdin);
+	len = strlen(dupes);
+	if (len > 1) {
+		rem_dupes(dupes, len);
+		printf("%s\n", dupes);
+	}
 
 	return 0;
 }
