c5e68d3c42eafc92fc14721c7048e5994835b7b2
Al Hunt <al@returnzero.org>
2017-08-05 23:28:53 +0800

    main: implemented the interface to clmul

--- 315-easy/src/main.c
+++ 315-easy/src/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <clmul.h>
 
+#define INPUT_MAX 255
+
 int main(int argc, char* argv[]) {
+	char input[INPUT_MAX];
+	int x, y, result;
+
+	printf("Enter two integers to carryless multiply: ");
+	fgets(input, INPUT_MAX, stdin);
+
+	if (sscanf(input, "%d %d", &x, &y) == 2) {
+		result = clmul(x, y);
+		printf("%d@%d=%d\n",x,y,result);
+	}
+	else
+		printf("Invalid input!\n");
+
 	return 0;
 }
