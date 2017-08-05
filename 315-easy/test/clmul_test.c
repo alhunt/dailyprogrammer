5175b735829c3f1f674be026019c399edc24142f
Al Hunt <al@returnzero.org>
2017-08-05 18:45:45 +0800

    clmul_test: added asserts for new clmul function

--- 315-easy/test/clmul_test.c
+++ 315-easy/test/clmul_test.c
@@ -1,12 +1,22 @@
 #include "minunit.h"
 #include "clmul.h"
 
-MU_TEST(test_minunit) {
-	mu_assert(0, "No Tests!");
+MU_TEST(test_clmul) {
+	mu_assert(clmul(1,2) == 2,      "Error: 1@2 should equal 2");
+	mu_assert(clmul(9,0) == 0,      "Error: 9@0 should equal 0");
+	mu_assert(clmul(6,1) == 6,      "Error: 6@1 should equal 6");
+	mu_assert(clmul(3,3) == 5,      "Error: 3@3 should equal 5");
+	mu_assert(clmul(2,5) == 10,     "Error: 2@5 should equal 10");
+	mu_assert(clmul(7,9) == 63,     "Error: 7@9 should equal 63");
+	mu_assert(clmul(13,11) == 127,  "Error: 13@11 should equal 127");
+	mu_assert(clmul(5,17)  == 85,   "Error: 5@17 should equal 85");
+	mu_assert(clmul(14,13) == 73,   "Error: 14@13 should equal 70");
+	mu_assert(clmul(19,1)  == 19,   "Error: 19@1 should equal 19");
+	mu_assert(clmul(63,63) == 1365, "Error: 63@63 should equal 1365");
 }
 
 MU_TEST_SUITE(test_suite) {
-	MU_RUN_TEST(test_minunit);
+	MU_RUN_TEST(test_clmul);
 }
 
 int main(int argc, char* argv[]) {
