1303cc824631dfc7d37165e199dbd85599b2ad12
Al Hunt <al@returnzero.org>
2017-08-05 23:00:02 +0800

    clmul: implemented function; fixed one of the tests

--- 315-easy/src/clmul.c
+++ 315-easy/src/clmul.c
@@ -1,5 +1,17 @@
 #include "clmul.h"
 
 int clmul(int x, int y) {
-	return 0;
+	int result = 0;
+
+	/* Think of the numbers in binary. Each bit is checked in 'x' and
+	 * if it is 1 then we need to get the value of 'y' shifted that many
+	 * bits XOR added to the result.
+	*/
+	for (int i = 0; x > 0; i++) {
+		if (x & 1)
+			result ^= y << i;
+		x >>= 1;
+	}
+
+	return result;
 }
--- 315-easy/test/clmul_test.c
+++ 315-easy/test/clmul_test.c
@@ -10,7 +10,7 @@ MU_TEST(test_clmul) {
 	mu_assert(clmul(7,9) == 63,     "Error: 7@9 should equal 63");
 	mu_assert(clmul(13,11) == 127,  "Error: 13@11 should equal 127");
 	mu_assert(clmul(5,17)  == 85,   "Error: 5@17 should equal 85");
-	mu_assert(clmul(14,13) == 73,   "Error: 14@13 should equal 70");
+	mu_assert(clmul(14,13) == 70,   "Error: 14@13 should equal 70");
 	mu_assert(clmul(19,1)  == 19,   "Error: 19@1 should equal 19");
 	mu_assert(clmul(63,63) == 1365, "Error: 63@63 should equal 1365");
 }
